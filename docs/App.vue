e7eecd4b5668e79796dd9a82b9773475e2a5bbf6
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-11-02 14:58:47 -0300

    Adding logic

--- docs/App.vue
+++ docs/App.vue
@@ -1,7 +1,15 @@
 <template lang='pug'>
-  .container.data-sticky-container
-    div(v-s)
+  .container
+    .box(v-s='stickyOptions')
       p Sticky Element
+
+    .box
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     | Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </template>
 
 <script>
@@ -12,12 +20,52 @@
 
     directives: {
       s: stickyDirectice
+    },
+
+    data() {
+      return {
+        stickyOptions: {
+          marginTop: 20,
+          forName: 0,
+          className: 'stuck'
+        },
+
+        stickyOptions2: {
+          marginTop: 20,
+          forName: 0,
+          className: 'stuck-2'
+        }
+      };
     }
   };
 </script>
 
 <style lang='scss'>
+  body {
+    font-family: sans-serif;
+  }
+
   .container {
     height: 2000px;
+  }
+
+  .box {
+    padding: 10px;
+    box-sizing: border-box;
+    border: 2px solid #3d3d3d;
+    width: 100%;
+    text-align: center;
+    line-height: 75px;
+    font-size: 20px;
+    background: #f3f3f3;
+    margin-bottom: 10px;
+  }
+
+  .stuck {
+    background: tomato !important;
+  }
+
+  .stuck-2 {
+    background: steelblue !important;
   }
 </style>
--- src/sticky.js
+++ src/sticky.js
@@ -2,10 +2,17 @@ import Sticky from 'sticky-js';
 
 export default {
   bind(val) {
-    this.el.className = 'sticky';
+    this.el.parentElement.setAttribute('data-sticky-container', '');
+    this.el.className += ' sticky';
   },
 
   update(val) {
+    if (val) {
+      if (val.marginTop) { this.el.setAttribute('data-margin-top', val.marginTop); }
+      if (val.forName) { this.el.setAttribute('data-sticky-for', val.forName); }
+      if (val.className) { this.el.setAttribute('data-sticky-class', val.className); }
+    }
+
     new Sticky('.sticky');
   }
 };
